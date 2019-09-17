00063fbb4fe1112f634e50fbd5961a874e6cad41
Ian Harisay <imharisa@nuskin.com>
2019-09-17 16:55:41 -0600

    LY-89 cleaned up pipeline scripts

--- scripts/pre-build.sh
+++ scripts/pre-build.sh
@@ -1,22 +1,6 @@
 echo "***********************************************************"
 echo "*                        Pre-Build                        *"
 echo "***********************************************************"
-echo "---------------"
-echo "|Install Grunt|"
-echo "---------------"
-yarn global add grunt
-echo "-----------------"
-echo "|Install Vue CLI|"
-echo "-----------------"
-yarn global add @vue/cli
-echo "---------------"
-echo "|Install Grunt|"
-echo "---------------"
-yarn global add grunt
-echo "-------------------"
-echo "|Install Grunt cli|"
-echo "-------------------"
-yarn global add grunt-cli
 echo "-----------------------"
 echo "|Install ESLint|"
 echo "-----------------------"
@@ -29,10 +13,6 @@ echo "--------------------------"
 echo "|Install of sonar-scanner|"
 echo "--------------------------"
 yarn global add sonar-scanner
-echo "--------------------------"
-echo "|Install of rosie-deployer|"
-echo "--------------------------"
-yarn global add @nuskin/rosiedeployer
 echo "--------------------------"
 echo "|Install package dependencies|"
 echo "--------------------------"
